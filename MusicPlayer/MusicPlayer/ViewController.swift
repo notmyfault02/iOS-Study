722e6dc64bd7821ab40e509cfbeed004f151621d
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-03 20:46:57 +0900

    Add MusicPlayer project

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 김영래 on 03/10/2019.
+//  Copyright © 2019 notmyfault02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
